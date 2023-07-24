f8f5a81c5485477e1e36b7fc365c95641cacf741
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-07-24 09:20:35 +0300

    added next auth google login and installed the DropdownMenu from shadcn-ui

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,27 @@
 import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
+import { getAuthSession } from "@/lib/auth";
 
-function Navbar() {
+async function Navbar() {
+  const session = await getAuthSession();
   return (
     <div className="fixed inset-x-0 top-0 z-[10] h-fit border-b border-zinc-300 bg-zinc-100 py-2">
       <div className="container mx-auto flex h-full max-w-7xl items-center justify-between gap-2">
         <Link href="/" className="flex items-center gap-2 ">
-          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6"/>
-          <p className="hidden text-zinc-700 text-sm font-medium md:block">Breadit</p>
+          <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
+          <p className="hidden text-sm font-medium text-zinc-700 md:block">
+            Breadit
+          </p>
         </Link>
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sing In
-        </Link>
+        {session ? (
+          // <pre>{JSON.stringify(session)}</pre>
+          <pre>session</pre>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sing In
+          </Link>
+        )}
       </div>
     </div>
   );
---
+++ src/lib/auth.ts
@@ -0,0 +1,75 @@
+import { db } from '@/lib/db'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { nanoid } from 'nanoid'
+import { NextAuthOptions, getServerSession } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/sign-in',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      // - here we create the session from the token, and we return the session
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+        session.user.username = token.username
+      }
+
+      return session
+    },
+
+    async jwt({ token, user }) {
+      // - here we create the token from the user and return the token
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      })
+
+      if (!dbUser) {
+        token.id = user!.id
+        return token
+      }
+
+      if (!dbUser.username) {
+        await db.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            username: nanoid(10),
+          },
+        })
+      }
+
+      // - all this custom data that you set on the token, you must set them manually on the sesssion inside the session callback, otherwise they won't be added to the session
+      // as the ideal case, is that you don't add every thing in the token to the session
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        username: dbUser.username,
+      }
+    },
+    redirect() {
+      return '/'
+    },
+  },
+}
+
+export const getAuthSession = () => getServerSession(authOptions)
