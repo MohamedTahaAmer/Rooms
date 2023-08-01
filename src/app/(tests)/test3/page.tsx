99537e984ab1261e670d43b9dfb99be950e90548
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-08-01 18:19:31 +0300

    Back in track after considring to switch from next due to no enough memory in this laptop

--- src/app/(tests)/test3/page.tsx
+++ src/app/(tests)/test3/page.tsx
@@ -1,20 +1,31 @@
 "use client";
+import { buttonVariants } from "@/components/ui/Button";
+import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const Page = () => {
-  const router = useRouter();
-  return (
-    <div className="text-2xl font-medium">
-      Not Protected
-      <button
-        onClick={() => {
-          router.push("/");
-        }}
-      >
-        home
-      </button>
-    </div>
+  // const router = useRouter();
+  const [test, setTest] = useState(true);
+  const button = (
+    <button
+      onClick={() => setTest((prev) => !prev)}
+      className={cn(
+        buttonVariants(),
+        "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2"
+      )}
+      // onClick={() => {
+      //   router.push("/");
+      // }}
+    >
+      home
+    </button>
   );
+  if (test) {
+    return <div>test {button}</div>;
+  } else {
+    return <div className="text-2xl font-medium">Not Protected {button}</div>;
+  }
 };
 
 export default Page;
--- ztest.js
+++
@@ -1,3 +0,0 @@
-str = "{-start-/search-end-}";
-const regexMatch = str.match(/(?<=-start-).*?(?=-end-)/); // /search
-const regex = regexMatch && regexMatch[0]
