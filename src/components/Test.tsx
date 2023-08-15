4c8ecfaa344a4dcfadf29dbd5bc961489dde4ab7
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-08-15 06:31:48 +0300

    redeploy after adding a secret env variable on vercel

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,30 +1,30 @@
-import { User } from "next-auth";
-import { FC } from "react";
-import { Avatar, AvatarFallback } from "./ui/Avatar";
-import Image from "next/image";
-import { AvatarProps } from "@radix-ui/react-avatar";
-import { Icons } from "./Icons";
+import { User } from 'next-auth';
+import { FC } from 'react';
+import { Avatar, AvatarFallback } from './ui/Avatar';
+import Image from 'next/image';
+import { AvatarProps } from '@radix-ui/react-avatar';
+import { Icons } from './Icons';
 
 interface UserAvatarProps extends AvatarProps {
-  user: Pick<User, "name" | "image">;
+  user: Pick<User, 'name' | 'image'>;
 }
 
 const Test: FC<UserAvatarProps> = ({ user }) => {
   return (
-    <Avatar className="mt-4">
+    <Avatar className='mt-4'>
       {user.image ? (
         <Image
           fill
-          sizes="100px"
+          sizes='100px'
           src={user?.image}
-          alt="profile picture"
-          referrerPolicy="no-referrer"
+          alt='profile picture'
+          referrerPolicy='no-referrer'
         />
       ) : (
         <AvatarFallback>
-          <span className="sr-only">{user?.name}</span>
-          <div className="relative h-8 w-8 bg-gray-200">
-            <Icons.user className="absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2" />
+          <span className='sr-only'>{user?.name}</span>
+          <div className='relative h-8 w-8 bg-gray-200'>
+            <Icons.user className='absolute left-1/2 top-1/2 h-4 w-4 -translate-x-1/2 -translate-y-1/2' />
           </div>
         </AvatarFallback>
       )}
