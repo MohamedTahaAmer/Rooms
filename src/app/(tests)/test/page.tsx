437ce5fb4e3160d7685a1b18abf05c8fd617addc
MohamedTahaAmer <MohamedTahaaAmer@gmail.com>
2023-08-15 07:22:04 +0300

    corrected the error

--- src/app/(tests)/test/page.tsx
+++ src/app/(tests)/test/page.tsx
@@ -1,23 +1,18 @@
-import Test from "@/components/Test";
-import { getAuthSession } from "@/lib/auth";
+import { getAuthSession } from '@/lib/auth';
 
 const Page = async () => {
   const session = await getAuthSession();
 
   return (
-    <section className="py-24">
-      <div className="container">
-        <h1 className="text-2xl font-bold">Protected Server</h1>
-        <div className="mx-auto w-fit text-2xl font-bold">
+    <section className='py-24'>
+      <div className='container'>
+        <h1 className='text-2xl font-bold'>Protected Server</h1>
+        <div className='mx-auto w-fit text-2xl font-bold'>
           {session?.user?.name}
         </div>
       </div>
-      <div className="test">
-        <Test
-          user={{ image: session?.user?.image!, name: session?.user?.name! }}
-        />
-      </div>
-      <p className="mt-8 w-fit rounded-xl bg-red-100 px-6 py-4">
+      <div className='test'></div>
+      <p className='mt-8 w-fit rounded-xl bg-red-100 px-6 py-4'>
         this is just comp
       </p>
     </section>
